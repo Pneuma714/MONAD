75b505b144a2035bd0b2646234229cca9658408f
Pneuma <syca0714@gmail.com>
2020-08-31 00:52:10 +0900

    .

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ client.on('message', message => {
 });
 
 client.login();
+
+// ==============================
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening at port ${process.env.PORT}`);
+});
